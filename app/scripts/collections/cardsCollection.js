65ecf6db39ce6ad6793eb178e527f51b670fe371
Shannon Riester <shannonriester@gmail.com>
2016-07-27 00:04:35 -0500

    changed image sources

--- app/scripts/collections/cardsCollection.js
+++ app/scripts/collections/cardsCollection.js
@@ -2,7 +2,7 @@ const cardsCollection = [
   {
   'id'    : 1,
   'name'  : 'The Windy',
-  'url'   : '/Users/shannonriester/Desktop/tiy/day-32/assignments/CardcaptorsTrading/app/assets/The_windy.jpg',
+  'url'   : 'https://dl.dropboxusercontent.com/u/19411356/day32-cardtradegame/The_windy.jpg',
   'magic' : 'Aerokenesis. The Windy\'s primary magic is to create strong winds. These winds can be used to blow away objects or bind creatures.',
   'elemental': true,
   'value' : 9,
@@ -13,7 +13,7 @@ const cardsCollection = [
   {
     'id'    : 2,
     'name'  : 'The Firey',
-    'url'   : '../../assets/The_firey.jpg',
+    'url'   : 'https://dl.dropboxusercontent.com/u/19411356/day32-cardtradegame/The_firey.jpg',
     'magic' : 'Pyrokinesis. Firey can create powerful fires from thin air or manipulate any existing flames. It can also regulate temperatures with thermokinesis.',
     'elemental': true,
     'value' : 9,
@@ -24,7 +24,7 @@ const cardsCollection = [
   {
     'id'    : 3,
     'name'  : 'The Fly',
-    'url'   : '../../assets/The_fly.jpg',
+    'url'   : 'https://dl.dropboxusercontent.com/u/19411356/day32-cardtradegame/The_fly.jpg',
     'magic' : 'Fly mainly grants the ability of flight. When used, the wings of the Master\'s Wand form at the staff\'s base.',
     'elemental': false,
     'value' : 6,
@@ -35,7 +35,7 @@ const cardsCollection = [
   {
     'id'    : 4,
     'name'  : 'The Jump',
-    'url'   : '../../assets/The_jump.jpg',
+    'url'   : 'https://dl.dropboxusercontent.com/u/19411356/day32-cardtradegame/The_jump.jpg',
     'magic' : 'Jump\'s magic can grant the master the ability to leap to great heights. When used, wings form around the feet/ankles of the master.',
     'elemental': false,
     'value' : 6,
@@ -46,7 +46,7 @@ const cardsCollection = [
   {
     'id'    : 5,
     'name'  : 'The Time',
-    'url'   : '../../assets/The_time.jpg',
+    'url'   : 'https://dl.dropboxusercontent.com/u/19411356/day32-cardtradegame/The_time.jpg',
     'magic' : 'Time has free control over the movement of time, having the ability to speed up, slow down, pause, and even cause recent events to repeat themselves.',
     'elemental': true,
     'value' : 10,
